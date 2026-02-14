6ca3c593cf9a1585eee4730eca961eb40f202335
AMendoza <allanmaleman@gmail.com>
2026-02-14 15:48:00 +0800

    FIX: adjust 404 for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://thinkglobalconsultants.com",
   base: "/",
   output: 'server',
+  adapter: vercel(),
 
   vite: {
     plugins: [tailwindcss()]
