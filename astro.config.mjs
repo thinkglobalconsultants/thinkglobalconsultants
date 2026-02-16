a57ad9553fce1505f362872a18b025266ac2518c
AMendoza <allanmaleman@gmail.com>
2026-02-16 17:31:55 +0800

    adjust config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,9 @@ import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
-
-const domain =
-  process.env.SITE_URL ||
-  (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : undefined);
-
 // https://astro.build/config
 export default defineConfig({
-  site: domain,
+  site: "https://thinkglobalconsultants.vercel.app",
   base: "/",
   output: 'server',
   adapter: vercel(),
